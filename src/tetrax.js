2c90be495b6af37bacbfb67ae9078a1467fc8383
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-25 00:23:37 -0300

    add new function

--- src/tetrax.js
+++ src/tetrax.js
@@ -5,6 +5,10 @@ class Tetrax  {
         this.brand = "Ciat-Lonbarde"
         this.keys = 4;
     }
+
+    newConnection() {
+        console.log('new connection');
+    }
 };
 
 export default Tetrax;
