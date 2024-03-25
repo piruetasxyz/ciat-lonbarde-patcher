173354fcb2cc8bbff6007a0e1cb30399a2974ee1
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-25 00:29:40 -0300

    add tetrax.js

--- src/tetrax.js
+++ src/tetrax.js
@@ -1,9 +1,10 @@
 // Tetrax
 class Tetrax  {
     constructor() {
-        this.name = "Tetrax";
-        this.brand = "Ciat-Lonbarde"
+        this.name = 'Tetrax';
+        this.brand = "Ciat-Lonbarde";
         this.keys = 4;
+        this.connections = new Array();
     }
 
     newConnection() {
