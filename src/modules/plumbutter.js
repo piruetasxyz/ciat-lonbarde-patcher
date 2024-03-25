d68b399b3125f75cdce481c26e42246fa906aba3
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-03-25 02:09:39 -0300

    add sections of plumbutter

--- src/modules/plumbutter.js
+++ src/modules/plumbutter.js
@@ -5,6 +5,46 @@ class Plumbutter {
     this.name = 'Plumbutter';
     this.brand = 'Ciat-Lonbarde';
     this.connections = new Array();
+    this.sections = [
+      new AVDog(),
+      new AVDog(),
+      new Gongue(),
+      new Gongue(),
+      new Ultrasound(),
+      new Ultrasound(),
+      new Deerhorn(),
+      new SnareDrum(),
+    ];
+  }
+}
+
+class AVDog {
+  constructor() {
+    this.name = 'AVDog';
+  }
+}
+
+class Gongue {
+  constructor() {
+    this.name = 'Gongue';
+  }
+}
+
+class Ultrasound {
+  constructor() {
+    this.name = 'Ultrasound';
+  }
+}
+
+class Deerhorn {
+  constructor() {
+    this.name = 'Deerhorn';
+  }
+}
+
+class SnareDrum {
+  constructor() {
+    this.name = 'SnareDrum';
   }
 }
 
