b30d64736e8646e4136444ebaac5c0ca381a64a5
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-03-08 09:57:03 -0300

    agregar modulos vacios de clicker y sidrax

---
+++ src/modules/clicker.js
@@ -0,0 +1,21 @@
+// clicker.js
+
+class Clicker {
+  constructor() {
+    this.name = 'Clicker';
+    this.brand = 'Ciat-Lonbarde';
+    this.connections = new Array();
+    this.sections = [
+      // new AVDog(),
+      // new AVDog(),
+      // new Gongue(),
+      // new Gongue(),
+      // new Ultrasound(),
+      // new Ultrasound(),
+      // new Deerhorn(),
+      // new SnareDrum(),
+    ];
+  }
+}
+
+export default Clicker;
---
+++ src/modules/sidrax.js
@@ -0,0 +1,21 @@
+// sidrax.js
+
+class Sidrax {
+  constructor() {
+    this.name = 'Sidrax';
+    this.brand = 'Ciat-Lonbarde';
+    this.connections = new Array();
+    this.sections = [
+      // new AVDog(),
+      // new AVDog(),
+      // new Gongue(),
+      // new Gongue(),
+      // new Ultrasound(),
+      // new Ultrasound(),
+      // new Deerhorn(),
+      // new SnareDrum(),
+    ];
+  }
+}
+
+export default Sidrax;
