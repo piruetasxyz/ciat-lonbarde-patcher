e94281e3f9402f817b1ad0c3ca3f0b9158e6849e
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-03-08 10:04:25 -0300

    borrador de sidrax basado en tetrax

--- src/modules/sidrax.js
+++ src/modules/sidrax.js
@@ -4,17 +4,65 @@ class Sidrax {
   constructor() {
     this.name = 'Sidrax';
     this.brand = 'Ciat-Lonbarde';
+    this.keysNumber = 7;
+    this.keys = new Array();
     this.connections = new Array();
-    this.sections = [
-      // new AVDog(),
-      // new AVDog(),
-      // new Gongue(),
-      // new Gongue(),
-      // new Ultrasound(),
-      // new Ultrasound(),
-      // new Deerhorn(),
-      // new SnareDrum(),
+    this.colors = [
+      'gray',
+      'red',
+      'blue',
+      'orange',
+      'green',
+      'orange',
+      'green',
     ];
+    for (let i = 0; i < this.keysNumber; i++) {
+      this.keys.push(new SidraxKey(i, this.colors));
+    }
+  }
+  newConnection() {
+    console.log('new connection');
+  }
+}
+
+class SidraxKey {
+  constructor(number, colors) {
+    this.number = number;
+
+    this.jacks = [
+      new SidraxJack(colors[0]),
+      new SidraxJack(
+        colors[1 + ((0 + number) % (colors.length - 1))],
+      ),
+      new SidraxJack(
+        colors[1 + ((1 + number) % (colors.length - 1))],
+      ),
+      new SidraxJack(
+        colors[1 + ((2 + number) % (colors.length - 1))],
+      ),
+      new SidraxJack(
+        colors[1 + ((3 + number) % (colors.length - 1))],
+      ),
+      new SidraxJack(
+        colors[1 + ((4 + number) % (colors.length - 1))],
+      ),
+      new SidraxJack(
+        colors[1 + ((5 + number) % (colors.length - 1))],
+      ),
+    ];
+  }
+}
+
+class SidraxJack {
+  constructor(color) {
+    this.color = color;
+    if (color === 'gray' || color === 'blue' || color === 'green') {
+      this.input = true;
+      this.output = false;
+    } else if (color === 'red' || color === 'orange') {
+      this.input = false;
+      this.output = true;
+    }
   }
 }
 
