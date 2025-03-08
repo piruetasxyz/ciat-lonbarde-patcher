1d3fa897e73f70aaf082a46b332d11abf0004b7a
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-03-08 10:00:59 -0300

    agregar secciones de clicker y sidrax

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // main.js
 
+import Clicker from './modules/clicker.js';
 import Cocoquantus from './modules/cocoquantus.js';
 import Deerhorn from './modules/deerhorn.js';
 import Peterlin from './modules/peterlin.js';
 import Plumbutter from './modules/plumbutter.js';
+import Sidrax from './modules/sidrax.js';
 import Tetrax from './modules/tetrax.js';
+
+const buttonClicker = document.getElementById('buttonAddClicker');
 
 const buttonCocoquantus = document.getElementById(
   'buttonAddCocoquantus',
@@ -18,7 +22,14 @@ const buttonPlumbutter = document.getElementById(
   'buttonAddPlumbutter',
 );
 
+const buttonSidrax = document.getElementById('buttonAddSidrax');
+
 const buttonTetrax = document.getElementById('buttonAddTetrax');
+
+buttonClicker.addEventListener('click', () => {
+  const clicker = new Clicker();
+  console.log(clicker);
+});
 
 buttonCocoquantus.addEventListener('click', () => {
   const cocoquantus = new Cocoquantus();
@@ -38,6 +49,11 @@ buttonPeterlin.addEventListener('click', () => {
 buttonPlumbutter.addEventListener('click', () => {
   const plumbutter = new Plumbutter();
   console.log(plumbutter);
+});
+
+buttonSidrax.addEventListener('click', () => {
+  const sidrax = new Sidrax();
+  console.log(sidrax);
 });
 
 buttonTetrax.addEventListener('click', () => {
